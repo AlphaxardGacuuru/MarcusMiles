98ea69adf4e2307fc16bfa16dbd95813a03180b6
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2024-11-23 18:15:44 +0300

    Moved Documents

--- resources/js/pages/admin/documents/requisitions/[id].js
+++ resources/js/pages/admin/documents/requisitions/[id].js
@@ -55,13 +55,20 @@ const form = (props) => {
 				<div className="offset-xl-2 col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12">
 					<div className="card bg-white p-5">
 						<div className="border-0 d-flex justify-content-between">
-							<div
-								className=""
-								style={{ width: "5em" }}>
-								<Img
-									src="/storage/img/favicon.png"
-									style={{ width: "100%", height: "auto" }}
-								/>
+							<div style={{ width: "7em" }}>
+								<div
+									className="mx-auto"
+									style={{ width: "5em" }}>
+									<Img
+										src="/storage/img/favicon.png"
+										style={{ width: "100%", height: "auto" }}
+									/>
+								</div>
+								<h6
+									className="text-center"
+									style={{ fontSize: "10px" }}>
+									RESEARCH. DESIGN & ENG. AUDIT PROJECT MANAGEMENT
+								</h6>
 							</div>
 
 							<div>
@@ -69,34 +76,37 @@ const form = (props) => {
 								<div className="p-2 text-center text-capitalize"></div>
 							</div>
 						</div>
-						<div className="card-body">
-							<div className="d-flex justify-content-between mb-4">
-								<div className="">
-									<h5 className="mb-1"></h5>
-								</div>
-								<div className="text-end">
-									<h5>
-										Form No:{" "}
-										<span className="text-dark fw-normal">
-											{requisition.code}
-										</span>
-									</h5>
-									<h5>
-										Project No:{" "}
-										<span className="text-dark fw-normal">
-											{requisition.projectCode}
-										</span>
-									</h5>
-									<h5>
-										Issue Date:{" "}
-										<span className="text-dark fw-normal">
-											{requisition.createdAt}
-										</span>
-									</h5>
-								</div>
-							</div>
 
-							<hr />
+						{/* First Header Start */}
+						<div className="d-flex justify-content-between">
+							<div className="">
+								<h5 className="mb-1"></h5>
+							</div>
+							<div className="text-end">
+								<h5>
+									Form No:{" "}
+									<span className="text-dark fw-normal">
+										{requisition.code}
+									</span>
+								</h5>
+								<h5>
+									Project No:{" "}
+									<span className="text-dark fw-normal">
+										{requisition.projectCode}
+									</span>
+								</h5>
+								<h5>
+									Issue Date:{" "}
+									<span className="text-dark fw-normal">
+										{requisition.createdAt}
+									</span>
+								</h5>
+							</div>
+						</div>
+						{/* First Header End */}
+
+						{/* Second Header Start */}
+						<div className="card-header border-0 my-4">
 							<h5 className="mb-1">
 								Project:{" "}
 								<span className="text-dark fw-normal">
@@ -110,43 +120,48 @@ const form = (props) => {
 								</span>
 							</h5>
 						</div>
+						{/* Second Header End */}
 
-						<div className="table-responsive-sm">
-							<table className="table bg-white">
-								<thead className="border-bottom">
-									<tr>
-										<th>No</th>
-										<th>BOQ Ref</th>
-										<th>Description</th>
-										<th>Unit</th>
-										<th>BOQ Price / PC</th>
-										<th>Purchase Price / PC</th>
-										<th>Total</th>
-										<th>Transport</th>
-										<th>Approval</th>
-									</tr>
-								</thead>
-								<tbody>
-									{requisition.boqGoods?.map((boqGood, key) => (
-										<tr key={key}>
-											<td>{key + 1}</td>
-											<td>{boqGood.boqCode}</td>
-											<td>{boqGood.description}</td>
-											<td>{boqGood.unit}</td>
-											<td>{boqGood.price}</td>
-											<td>{boqGood.price}</td>
-											<td>{boqGood.total}</td>
-											<td>{boqGood.transport}</td>
-											<td>{boqGood.approval}</td>
-											<td></td>
+						<div className="centered-grey-background">
+							{/* Table Start */}
+							<div className="table-responsive-sm">
+								<table className="table bg-white">
+									<thead className="border-bottom">
+										<tr>
+											<th>No</th>
+											<th>BOQ Ref</th>
+											<th>Description</th>
+											<th>Unit</th>
+											<th>BOQ Price / PC</th>
+											<th>Purchase Price / PC</th>
+											<th>Total</th>
+											<th>Transport</th>
+											<th>Approval</th>
 										</tr>
-									))}
-								</tbody>
-							</table>
+									</thead>
+									<tbody>
+										{requisition.boqGoods?.map((boqGood, key) => (
+											<tr key={key}>
+												<td>{key + 1}</td>
+												<td>{boqGood.boqCode}</td>
+												<td>{boqGood.description}</td>
+												<td>{boqGood.unit}</td>
+												<td>{boqGood.price}</td>
+												<td>{boqGood.price}</td>
+												<td>{boqGood.total}</td>
+												<td>{boqGood.transport}</td>
+												<td>{boqGood.approval}</td>
+												<td></td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
+							{/* Table End */}
 						</div>
 
 						{/* First Footer Start */}
-						<div className="card-footer d-flex justify-content-between bg-white border-0">
+						<div className="d-flex justify-content-betwee border-0 my-2">
 							<div className="text-start">
 								<h6>Ordered By (Tradesman): {requisition.createdByName}</h6>
 								<h6>Checked By (Foreman): {requisition.checkedByName}</h6>
